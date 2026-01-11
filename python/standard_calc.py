9f0a501f81df307b6d29257f0e9ab91495afc0c5
arkumar57 <arkumar57@outlook.com>
2026-01-10 20:10:28 -0800

    Sailbot_Assessment_Final_Commit

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,22 +11,60 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+
+    upper_bound = 180
+    lower_bound = -180
+
+    # Repeatedly shifts the angle until it is between [-180, 180)
+    while (angle > upper_bound or angle < lower_bound):
+        if (angle > upper_bound):
+            angle = angle - 360
+        else:
+            angle = angle + 360
+    return angle
+
+
+def bound_to_0_to_360(angle):
+
+    """ Helper Function
+    Bounds the provided angle to the range [0, 360) degrees.
+    Args:
+        angle (float): Input angle in degrees.
+    Returns:
+        float: Angle bounded to [0, 360).
+    """
+    # Shifts angle upwards if negative
+    while (angle < 0):
+        angle = angle + 360
+    # Shifts angle downwards if >= 360
+    while (angle >= 360):
+        angle = angle - 360
+    return angle
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
     """Determines whether an angle is between two other angles.
-
     e.g.)
         is_angle_between(0, 45, 90) = True
         is_angle_between(45, 90, 270) = False
-
     Args:
         first_angle (float): The first bounding angle in degrees.
         middle_angle (float): The angle in question in degrees.
         second_angle (float): The second bounding angle in degrees.
-
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+    # bounds first_angle, middle_angle, second_angle between [0, 360)
+    first_angle = bound_to_0_to_360(first_angle)
+    middle_angle = bound_to_0_to_360(middle_angle)
+    second_angle = bound_to_0_to_360(second_angle)
+    # computes counter clockwise angles from first_angle to middle and second
+    angle_from_first_to_middle = (middle_angle - first_angle) % 360
+    angle_from_first_to_second = (second_angle - first_angle) % 360
+    # Check if angle between first_angle and second_angle is not reflex angle
+    if (angle_from_first_to_second <= 180):
+        # return true if middle_angle is between first_angle and second_angle
+        return angle_from_first_to_middle <= angle_from_first_to_second
+    else:
+        # return true if middle_angle lies on shorter arc that cycles around 0 degrees
+        return angle_from_first_to_middle >= angle_from_first_to_second
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -1,4 +1,4 @@
-from standard_calc import bound_to_180, is_angle_between
+from standard_calc import bound_to_180, is_angle_between, bound_to_0_to_360
 
 
 """ Tests for bound_to_180() """
@@ -8,8 +8,66 @@ def test_bound_basic1():
     assert bound_to_180(0) == 0
 
 
+def test_bound_edge_case():
+    assert bound_to_180(180) == 180
+    assert bound_to_180(-180) == -180
+
+
+def test_bound_positive_angle_less_than_180():
+    assert bound_to_180(110) == 110
+
+
+def test_bound_positive_angle_greater_than_180():
+    assert bound_to_180(220) == -140
+    assert bound_to_180(380) == 20
+
+
+def test_bound_negative_angle_greater_than_negative_180():
+    assert bound_to_180(-90) == -90
+
+
+def test_bound_negative_angle_less_than_negative_180():
+    assert bound_to_180(-220) == 140
+    assert bound_to_180(-380) == -20
+
+
+""" Tests for bound_to_0_to_360() """
+
+
+def test_bound_to_0_to_360_Case1():
+    assert bound_to_0_to_360(200) == 200
+
+
+def test_bound_to_0_to_360_Case2():
+    assert bound_to_0_to_360(0) == 0
+
+
+def test_bound_to_0_to_360_Case3():
+    assert bound_to_0_to_360(540) == 180
+
+
+def test_bound_to_0_to_360_Case4():
+    assert bound_to_0_to_360(-180) == 180
+
+
+def test_bound_to_0_to_360_Case5():
+    assert bound_to_0_to_360(-500) == 220
+
+
 """ Tests for is_angle_between() """
 
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+
+
+def test_between_basic2():
+    assert not is_angle_between(40, 80, 300)
+
+
+def test_between_basic3():
+    assert is_angle_between(20, 60, 200)
+
+
+def test_between_basic4():
+    assert is_angle_between(20, 30, 150)
